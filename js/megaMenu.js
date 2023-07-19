1f50b9ab432f6daa2f051ff6cf058ef4c123bdf3
MinaRomany53 <minaromany190@gmail.com>
2023-07-19 23:35:46 +0300

    Update HomePage

---
+++ js/megaMenu.js
@@ -0,0 +1,5 @@
+document.querySelector(".account__image").addEventListener("click", (e) => {
+  const accountMenu = document.querySelector(".account__menu");
+  accountMenu.classList.toggle("account__menu-hidden");
+  accountMenu.classList.toggle("account__menu-show");
+});
